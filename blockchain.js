bcc9cd325e83f40449e29ba75711453a195ef977
peter.mckinney <peter.mckinney@cis.ntt.com>
2019-09-30 11:32:52 +0100

    added protection against difficulty jumping

--- blockchain.js
+++ blockchain.js
@@ -6,21 +6,21 @@ class Blockchain {
         this.chain = [Block.genesis()];
     }
 
-    addBlock({data}) {
+    addBlock( {data} ) {
         const newBlock = Block.mineBlock({
-            lastBlock: this.chain[this.chain.length - 1],
+            lastBlock: this.chain[ this.chain.length - 1 ],
             data: data
         });
 
         this.chain.push(newBlock);
     }
 
-    replaceChain(chain) {
-        if(chain.length <= this.chain.length) {
+    replaceChain( chain ) {
+        if ( chain.length <= this.chain.length ) {
             console.error('the incoming chain must be longer than the current chain');
             return;
         }
-        if(!Blockchain.isValidChain(chain)) {
+        if ( !Blockchain.isValidChain(chain) ) {
             console.error('the incoming chain must be valid');
             return;
         }
@@ -29,15 +29,25 @@ class Blockchain {
         this.chain = chain;
     }
 
-    static isValidChain(chain) {
-        if (JSON.stringify(chain[0]) !== JSON.stringify(Block.genesis())) return false;
+    static isValidChain( chain ) {
+        if ( JSON.stringify(chain[ 0 ]) !== JSON.stringify(Block.genesis()) ) {
+            return false;
+        }
 
-        for (let i = 1; i < chain.length; i++) {
-            const {timestamp, lastHash, hash, data, nonce, difficulty} = chain[i];
-            const actualLastHash = chain[i - 1].hash;
+        for ( let i = 1; i < chain.length; i++ ) {
+            const lastDifficulty = chain[ i - 1 ].difficulty;
+            const {timestamp, lastHash, hash, data, nonce, difficulty} = chain[ i ];
+            if ( Math.abs(lastDifficulty - difficulty) > 1 ) {
+                return false;
+            }
+            const actualLastHash = chain[ i - 1 ].hash;
+            if ( lastHash !== actualLastHash ) {
+                return false;
+            }
             const validatedHash = cryptoHash(timestamp, lastHash, data, nonce, difficulty);
-            if (lastHash !== actualLastHash) return false;
-            if (hash !== validatedHash) return false;
+            if ( hash !== validatedHash ) {
+                return false;
+            }
         }
         return true;
     }
