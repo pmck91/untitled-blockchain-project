ae4c5854affbf99e39bdd26974b2f0d6f362d6d0
peter.mckinney <peter.mckinney@cis.ntt.com>
2019-09-30 13:55:00 +0100

    added basic api endpoint for listing and mining blocks

---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const Blockchain = require('./blockchain');
+
+const app = express();
+const blockchain = new Blockchain();
+
+app.use(bodyParser.json());
+
+// --------------------- END POINTS -----------------------
+
+app.get('/api/blocks', ( req, res ) => {
+    res.json(blockchain.chain);
+});
+
+app.post('/api/mine', ( req, res ) => {
+    const {data} = req.body;
+
+    blockchain.addBlock({data});
+
+    res.redirect('/api/blocks');
+});
+
+
+const port = 3000;
+app.listen(port, () => {
+    console.log(`Application started listening on port: ${port}`);
+});
